dab42083db68065f1371ea82bd5f6f9041d67176
Eugene Krevenets <hyzhak@ya.ru>
2013-11-24 00:50:29 +0100

    return simple json with ip and location of user on request to root '/'

---
+++ main.py
@@ -0,0 +1,49 @@
+import webapp2
+from webapp2_extras import json
+
+
+class MainHandler(webapp2.RequestHandler):
+    '''
+    handle request to root and share json with user ip, and location
+    '''
+
+    def get(self):
+        request = self.request
+        response = self.response
+
+        location = [coord.strip() for coord in str(request.headers.get("X-AppEngine-CityLatLong")).split(',')]
+
+        #allow all CORS requests
+        self.response.headers.add_header('Access-Control-Allow-Origin', '*')
+
+        response.headers['Content-Type'] = 'application/json'
+        response.write(json.encode({
+            'ip': str(request.remote_addr),
+            'country': str(request.headers.get("X-AppEngine-Country")),
+            'region': str(request.headers.get("X-AppEngine-Region")),
+            'city': str(request.headers.get("X-AppEngine-City")),
+            'coords': {
+                'latitude': safe_list_get(location, 0, 0),
+                'longitude': safe_list_get(location, 1, 0)
+            }
+        }))
+
+
+def safe_list_get(l, idx, default=None):
+    '''
+    safe get element from list
+
+    :param l:
+    :param idx:
+    :param default:
+    :return:
+    '''
+
+    if len(l) > idx:
+        return l[idx]
+    else:
+        return default
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
